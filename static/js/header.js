69c2a50315ff8a72d1226b6c0449747e0fd9eae0
Herresuelo <herresuelo3@gmail.com>
2024-07-11 20:13:46 -0300

    frontend final

---
+++ static/js/header.js
@@ -0,0 +1,6 @@
+document.getElementById("header").innerHTML= `
+<nav class="menu">
+            <a href="../auspiciantes.html">Tabla</a>
+            <a href="../templates/ingresar_producto.html">Registrar AUSPICIANTES</a>
+</nav>
+`
